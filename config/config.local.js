4a217dedbd47cd03b45ea640babdd2856eec2377
caoli <cl98914@alibaba-inc.com>
2018-10-23 19:22:22 +0800

    feat: implement webpack config read

--- config/config.local.js
+++ config/config.local.js
@@ -1,5 +1,6 @@
 'use strict';
-module.exports = () => {
+const utils = require('../lib/utils');
+module.exports = app => {
   const exports = {};
 
   exports.view = {
@@ -13,6 +14,10 @@ module.exports = () => {
   exports.development = {
     watchDirs: [], // 指定监视的目录（包括子目录），当目录下的文件变化的时候自动重载应用，路径从项目根目录开始写
     ignoreDirs: [ 'app/web', 'public', 'config/manifest.json' ], // 指定过滤的目录（包括子目录）
+  };
+
+  exports.webpack = {
+    webpackConfigList: utils.getWebpackConfig(app),
   };
 
   return exports;
