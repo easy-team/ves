a2a3512216ef41e6a013082083277b546a2a798c
sky <hubcarl@126.com>
2020-02-24 22:54:54 +0800

    fix: npm run dev not run

--- bin/cli.js
+++ bin/cli.js
@@ -8,7 +8,7 @@ const cliFilePath = path.join(process.cwd(), 'node_modules', 'ves-cli');
 // fix when production mode, ves-cli not exists, start app error by ves start
 if (fs.existsSync(cliFilePath)) {
   const VesCLI = require(cliFilePath);
-  new VesCLI.Command().start();
+  new VesCLI.Command().run();
 } else {
   new EggScriptCommand().start();
 }
